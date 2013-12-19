d60cefbd39a0e14171ba2246f1aebc9debbbb545
Chris Armstrong <chris.armstrong@socrata.com>
2013-12-18 18:31:25 -0800

    Remove cluster test from server_test -- we are now checking this in cluster_test

--- files/default/test/server_test.rb
+++ files/default/test/server_test.rb
@@ -66,20 +66,4 @@ describe_recipe "couchbase::server" do
     end
   end
 
-  describe "default Couchbase cluster" do
-    let(:cluster) do
-      couchbase_cluster(node["couchbase"]["server"]["cluster"], {
-        :username => node["couchbase"]["server"]["username"],
-        :password => node["couchbase"]["server"]["password"]
-      })
-    end
-
-    it "exists" do
-      assert cluster.exists
-    end
-
-    it "has its memory quota configured" do
-      cluster.must_have :memory_quota_mb, node["couchbase"]["cluster"]["memory_quota_mb"]
-    end
-  end
 end
