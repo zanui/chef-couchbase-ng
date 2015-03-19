949d594f5883c182c4fd71621a883e4ded539f62
Enrico Stahn <mail@enricostahn.com>
2015-03-19 18:25:22 +1100

    Added source_url and issue_url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          "MIT"
 description      "Installs and configures Couchbase Server."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.3.1"
+source_url       "https://github.com/zanui/chef-couchbase-ng"
+issues_url       "https://github.com/zanui/chef-couchbase-ng/issues"
 
 %w{debian ubuntu centos redhat oracle amazon scientific windows}.each do |os|
   supports os
