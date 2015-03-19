b7615e005091b1d748d7f1133c17ef8386628f19
Enrico Stahn <mail@enricostahn.com>
2015-03-19 18:26:33 +1100

    Bump version to 1.3.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "engineering@zanui.com.au"
 license          "MIT"
 description      "Installs and configures Couchbase Server."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.3.1"
+version          "1.3.2"
 source_url       "https://github.com/zanui/chef-couchbase-ng"
 issues_url       "https://github.com/zanui/chef-couchbase-ng/issues"
 
